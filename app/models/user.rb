d70c1e3b615d00529fb9dcb06f0613749479d0ad
Trevor Nodland <trevor.nodland@gmail.com>
2019-03-28 12:09:04 -0600

    adds authorization method to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+
+  def self.authenticate(email, password)
+    user = User.where(email: email)
+    if user.password == password
+      session.create(user: user)
+    else
+      redirect_to login_path
+      flash[:failure] = "Invalid credentials"
+    end
+  end
 end
